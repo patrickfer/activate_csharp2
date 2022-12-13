31400ab7fade2f0f694d81ac4fe9ac73c683c8af
Patrick Fernando <patrickfernando1234@gmail.com>
2022-12-13 16:38:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] vect = new Student[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= n; i++)
+
+            {
+                Console.WriteLine("Rent #" + i);
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");  
+                int room = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                vect[room] = new Student(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms: ");
+            
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect [i]);
+                }
+            }
+            Console.ReadLine();
+         
+               
+
+
+
+
+              
+
+
+
+        }
+    }
+}
+
---
+++ Student.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Student (string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+
+    }
+}
